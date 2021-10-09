19c0e0e1858932b01ee2bd912dc1ce44fa052121
TaraRhoseyn <taragulwell@gmail.com>
2021-10-09 11:37:02 +0000

    Add two blank lines in between functions in app.py for PEP8 compliance

--- app.py
+++ app.py
@@ -14,13 +14,16 @@ to the decorator directly above it
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(
