994ddfc5605990711fb6dea3203b3eafd3f16702
TaraRhoseyn <taragulwell@gmail.com>
2021-10-09 11:23:47 +0000

    docs: Add comment explaining the @app.route() decorator

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+"""
+Whenever 'route' is called (the directory)
+the index() function is also called due to 
+@app decorator
+"""
 @app.route("/")
 def index():
-    return "<h1>Hello</h1> <h2>world</h2>"
+    return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(
